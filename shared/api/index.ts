19166d521b88c69d57ef1ce607b5d7bea8f0e884
Bruno Adam <bruno.adam@pm.me>
2025-08-10 10:44:44 +0200

    feat(shared/api): export additional modules from index

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -2,3 +2,6 @@
 export * from "./client/supabaseClient";
 export * from "./activities";
 export * from "./activitiesCategories";
+export * from "./notes";
+export * from "./states";
+export * from "./timeslices";
