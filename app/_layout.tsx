91e6030f7eda115216108ca2084a93ac967c567d
Bruno Adam <bruno.adam@pm.me>
2025-08-16 11:34:42 +0200

    feat: restructure project configuration from Expo templates to solve SDK issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,38 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 import { SECRETS } from "@/shared/constants/SECRETS";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
   return (
-    <ClerkProvider
-      publishableKey={SECRETS.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-      tokenCache={tokenCache}
-    >
-      <Slot />
-    </ClerkProvider>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ClerkProvider
+        publishableKey={SECRETS.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+        tokenCache={tokenCache}
+      >
+        <Slot />
+      </ClerkProvider>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
---
+++ hooks/useThemeColor.ts
@@ -0,0 +1,21 @@
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { COLORS } from '@/shared/constants/COLORS';
+
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof COLORS.light & keyof typeof COLORS.dark
+) {
+  const theme = useColorScheme() ?? 'light';
+  const colorFromProps = props[theme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return COLORS[theme][colorName];
+  }
+}
